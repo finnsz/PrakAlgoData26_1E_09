405a44f1ab4f1f7eff026a177350b65457efa6d7
finnsz <xpvppr0@gmail.com>
2026-04-23 08:39:20 +0700

    2

---
+++ Jobsheet7/Mahasiswa09.java
@@ -0,0 +1,17 @@
+package Jobsheet7;
+
+public class Mahasiswa09 {
+    String nim, nama, kelas;
+    int nilai;
+
+    Mahasiswa09(String nama, String nim, String kelas){
+        this.nama = nama;
+        this.nim = nim;
+        this.kelas = kelas;
+        nilai = -1;
+    }
+
+    void tugasDinilai(int nilai) {
+        this.nilai = nilai;
+    }
+}
