cb0f7fdc496427096c7df43e7fc2e9a7be023375
finnsz <xpvppr0@gmail.com>
2026-04-23 08:57:47 +0700

    2.2

--- Jobsheet7/MahasiswaDemo09.java
+++ Jobsheet7/MahasiswaDemo09.java
@@ -37,6 +37,8 @@ public class MahasiswaDemo09 {
                     int nilai = sc.nextInt();
                     dinilai.tugasDinilai(nilai);
                     System.out.printf("Nilai Tugas %s adalah %d\n", dinilai.nama, nilai);
+                    String biner = stack.konversiDesimalkeBiner(nilai);
+                    System.out.println("Nilai Biner Tugas: " + biner);
                 }
                 break;
 
